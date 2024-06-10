08610479c08def263b8f9bc308194f74d3ddb42a
Robert Beisicht <robert@beisicht.nl>
2024-06-10 10:03:33 +0200

    Add test for Add Function

---
+++ Assertive.Tests/Functions/AddTests.cs
@@ -0,0 +1,26 @@
+﻿using Assertive.Functions;
+using Assertive.Types;
+
+namespace Assertive.Tests.Functions
+{
+    public class AddTests
+    {
+        [Fact]
+        public async Task AddShouldAddNewEntry()
+        {
+            var sut = new Add();
+            var dic = new DictionaryValue();
+            var context = new FunctionContext() { FilePath = "c:\\unittest.ass" };
+            var values = new List<Value>
+            {
+                dic,
+                new StringValue("key"),
+                new StringValue("value")
+            };
+
+            await sut.Execute(values, context);
+
+            Assert.NotEmpty(dic.GetEntries());
+        } 
+    }
+}
